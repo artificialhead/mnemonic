4706411154f2dfcc51ed83ee3f6a9eb4baa70a67
Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>
2017-01-19 23:01:47 +0800

    added put method
    Signed-off-by: Ahmad Shah Hafizan Hamidin <ahmadshahhafizan@gmail.com>

--- lib/mnemonic.ex
+++ lib/mnemonic.ex
@@ -2,9 +2,9 @@ defmodule Mnemonic do
   @ets_table :mnemonic
   @driver Application.get_env(:mnemonic, :driver)
 
-  def start do
+  def load do
     apply(@driver, :load, [])
-    |> cache_memory()
+    |> create_cache()
     {:ok}
   end
 
@@ -16,32 +16,43 @@ defmodule Mnemonic do
   """
   def get(key, default \\ nil) do
     if nested_elem?(key) do
-      keys = String.split(key, ".")
-      try do
-        memory = find_from_memory(List.first(keys))
-
-        value = get_in(memory, List.delete_at(keys, 0))
-        if !is_nil(value), do: value, else: default
-      rescue
-        FunctionClauseError -> default
-      end
+      [key | nested_keys] = String.split(key, ".")
     else
-      memory = find_from_memory(key)
-      if !is_nil(memory), do: memory, else: default
+      nested_keys = key
     end
+
+    find_from_memory(key)
+    |> traverse_memory(nested_keys, default)
   end
 
-  def put(key, value) do
+  @doc """
+  Insert or update the stored memory. If the key does not exists, it will insert a new key and value into the memory.
 
+  ##Example
+      Mnemonic.put "foo.bar", "new_value"
+  """
+  def put(key, value) do
+    if !is_nil(get(key)) do
+      replace_existing_memory(key, value)
+      {:ok}
+    else
+      cache_memory(key, value)
+      {:ok}
+    end
   end
 
   def forget(key) do
 
   end
 
+  @doc """
+  Flush the ETS table. This will delete all stored memories inside the ETS table.
+
+  ##Example
+      Mnemonic.flush
+  """
   def flush do
     :ets.delete_all_objects(@ets_table)
-
     {:ok}
   end
 
@@ -49,16 +60,26 @@ defmodule Mnemonic do
 
   end
 
-  defp cache_memory(memories) do
-    :ets.new(@ets_table, [:set, :protected, :named_table])
+  defp create_cache(memories) do
+    if :ets.info(@ets_table) == :undefined do
+      :ets.new(@ets_table, [:set, :protected, :named_table])
+    end
+
     Enum.each(memories, fn (memory) ->
       key =
         elem(memory, 0)
         |> to_string()
       value = elem(memory, 1)
-      :ets.insert(@ets_table, {key, value})
+
+      cache_memory(key, value)
     end)
   end
+
+  def cache_memory(key, value),
+    do: :ets.insert(@ets_table, {key, value})
+
+  defp nested_elem?(key),
+    do: String.contains?(key, ".")
 
   defp find_from_memory(key) do
     try do
@@ -70,6 +91,35 @@ defmodule Mnemonic do
     end
   end
 
-  defp nested_elem?(key),
-    do: String.contains?(key, ".")
+  defp traverse_memory(memory, key, default) when is_list(key) do
+    try do
+      value = get_in(memory, key)
+      if !is_nil(value), do: value, else: default
+    rescue
+      FunctionClauseError -> default
+    end
+  end
+  defp traverse_memory(nil, _, default),
+    do: default
+  defp traverse_memory(memory, key, default) do
+    if !is_nil(memory), do: memory, else: default
+  end
+
+  defp replace_existing_memory(key, value) do
+    if nested_elem?(key) do
+      [key | nested_keys] = String.split(key, ".")
+    else
+      nested_keys = key
+    end
+
+    find_from_memory(key)
+    |> replace(nested_keys, value, key)
+  end
+
+  defp replace(memory, key, value, root_key \\ nil) when is_list(key) do
+    new_memory = put_in(memory, key, value)
+    cache_memory(root_key, new_memory)
+  end
+  defp replace(memory, key, value, _),
+    do: cache_memory(key, value)
 end
